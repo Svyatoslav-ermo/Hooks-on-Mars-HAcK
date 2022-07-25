5953ba2c4efa3a6196cff35af473202ce4e47f15
Svyatoslav-ermo <Svatoslav1312@gmail.com>
2022-07-24 21:00:11 -0700

    Project Update

--- Project/ESP32/ESP32.ino
+++ Project/ESP32/ESP32.ino
@@ -5,15 +5,19 @@
 #error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
 #endif
 
-Servo myservo;
+Servo myservoLeft;
+Servo myservoRight;
 // Create Servo object
-int servoPin = 13; // Define Servo pin
+int servoPinLeft = 13; // Define Servo pin
+int servoPinRight = 12;
 int servoAngle = 10; // Set Servo angle
 BluetoothSerial SerialBT; //declares the Bluetooth object
 
 void setup() {
-  myservo.attach(servoPin, 500, 2400); // attach servo
-  myservo.write(servoAngle);
+  myservoLeft.attach(servoPinLeft, 500, 2400); // attach servo
+  myservoLeft.write(servoAngle);
+  myservoRight.attach(servoPinRight, 500, 2400);
+  myservoRight.write(servoAngle);
   
   Serial.begin(115200); // Serial monitor
   Serial1.begin(9600); // Arduino Communication
@@ -24,7 +28,7 @@ void setup() {
 
 
 
-int servoInc = 50; // Increments to change servo angle
+int servoInc = 10; // Increments to change servo angle
 
 char readIn; // Define Bluetooth char variable
 
@@ -78,52 +82,44 @@ void loop()
     if (readIn == 'o' && servoAngle < 180) 
     { // if input is o and servo angle is less than 180 degrees
       servoAngle += servoInc; // Increment angle
-      myservo.write(servoAngle); // Write new angle to servo
+      myservoLeft.write(servoAngle); // Write new angle to servo
       Serial.println("Inside increment angle");
       readIn = '\0';
-  }
-     // myservoLeft.write(100); 
-    if (readIn == 'l' && servoAngle > 10) { // else if input is o and servo angle is less than 180 degrees
+    }
+    if (readIn == 'p' && servoAngle > 10) 
+    {// else if input is o and servo angle is less than 180 degrees
       servoAngle -= servoInc; // Decrement angle
-      myservo.write(servoAngle); // Write new angle to servo
+      myservoLeft.write(servoAngle); // Write new angle to servo
       Serial.println("Inside increment angle");
       readIn = '\0';
     }
 
-//    if (readIn == '1') {
-//    Serial1.print('1');
-//    readIn = '\0';
-//  }
-//
-//    if (readIn == '2') {
-//    Serial1.print('2');
-//    readIn = '\0';
-//  }
-//
-//    if (readIn == '3') {
-//    Serial1.print('3');
-//    readIn = '\0';
-//  }
-//
-//    if (readIn == '4') {
-//    Serial1.print('4');
-//    readIn = '\0';
-//  }
-//
-//    if (readIn == '5') {
-//    Serial1.print('5');
-//    readIn = '\0';
-//  }
-//
-//    if (readIn == '0') {
-//    Serial1.print('0');
-//    readIn = '\0';
-//  }
 
-   delay(100);
+    }
+    if (readIn == 'k' && servoAngle < 180) { // else if input is o and servo angle is less than 180 degrees
+      servoAngle += servoInc; // Decrement angle
 
-    //SerialBT.println("ON!");
+    if (readIn == 'k' && servoAngle < 180) 
+    { // else if input is o and servo angle is less than 180 degrees
+      servoAngle += servoInc; // Decrement angle
+      myservoRight.write(servoAngle); // Write new angle to servo
+      Serial.println("Inside increment angle");
+      readIn = '\0';
+    }
+    if (readIn == 'l' && servoAngle > 10) 
+    { // else if input is o and servo angle is less than 180 degrees
+      servoAngle -= servoInc; // Decrement angle
 
-    //SerialBT.println("OFF!");
+      myservoRight.write(servoAngle); // Write new angle to servo
+      Serial.println("Inside increment angle");
+      readIn = '\0';
+    }
 
-}
+    if (readIn == 'l' && servoAngle > 10) { // else if input is o and servo angle is less than 180 degrees
+      servoAngle -= servoInc; // Decrement angle
+      myservoRight.write(servoAngle); // Write new angle to servo
+      Serial.println("Inside increment angle");
+      readIn = '\0';
+    }
+    
+   delay(20);
